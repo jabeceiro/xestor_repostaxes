41a53355faec6f26f24b0aa64a15c54ddf4410ae
jabeceiro <jabeceirocarro@gmail.com>
2026-01-18 00:18:50 +0100

    MOD:main.py - Creación de docstrings e revisión de código

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+"""
+Módulo principal da aplicación de xestión de repostaxes.
+Encárgase de cargar os datos, mostrar o menú e xestionar as opcións do usuario.
+"""
+
 from app import io
 
 
 def main():
+    """
+    Punto de entrada da aplicación.
+    Xestiona o menú principal e as accións do usuario.
+    """
     repostaxes = io.cargar_repostaxes()
 
     while True:
-        print()
+        print()     # liña en branco de separación
         io.mostrar_menu()
         opcion = io.pedir_opcion_menu()
+        
         match opcion:
             case 1:
                 io.rexistrar_repostaxe_desde_input(repostaxes)
@@ -23,10 +33,12 @@ def main():
                 io.gardar_repostaxes(repostaxes)
                 print("\n✅ Datos gardados.")
             case 0:
-                print("Saíndo...")
+                print("\nSaíndo da aplicación...")
+                print("Ata logo!")
                 break
             case _:
-                print("\n❌ Opción non válida.","Introduce un número do 0 o 6.",sep="\n")
+                print("\n❌ Opción non válida.")
+                print("Introduce un número do 0 ao 6.",sep="\n")
 
 
 if __name__ == "__main__":
